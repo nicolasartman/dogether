793b85346dd5620287fa09f3aeb9decede1a89c5
Ben Jaffe <jaffe.ben@gmail.com>
2015-03-21 21:56:50 -0700

    gulp cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -33,11 +33,11 @@ var RELEASE = !!argv.release;
 
 
 gulp.task('browserify', function() {
-	bundleJS(false)
+  bundleJS(false)
 });
 
 gulp.task('browserify-watch', function() {
-	bundleJS(true);
+  bundleJS(true);
 });
 
 // Host static files and automatically
@@ -45,193 +45,190 @@ gulp.task('browserify-watch', function() {
 gulp.task('connect', function() {
   connect.server({
     root: 'build',
-		port: 9090,
+    port: 9090,
     livereload: true
   });
 });
 
 function bundleJS(watch) {
-	var bundler = browserify({
-		entries: ['./src/app.js'], // Just our entry point
-		transform: [reactify], // We want to convert JSX to normal javascript
-		debug: true, // give us sourcemapping
-		cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
-	});
+  var bundler = browserify({
+    entries: ['./src/app.js'], // Just our entry point
+    transform: [reactify], // We want to convert JSX to normal javascript
+    debug: true, // give us sourcemapping
+    cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+  });
 
 
-	if (watch) {
-		var watcher = watchify(bundler);
+  if (watch) {
+    var watcher = watchify(bundler);
 
-		watcher.transform(babelify);
+    watcher.transform(babelify);
 
-		return watcher
-			.on('update', function() {
-				var updateStart = Date.now();
-				console.log('Let\'s update!');
-				var newBundle = watcher.bundle();
+    return watcher
+      .on('update', function() {
+        var updateStart = Date.now();
+        console.log('Let\'s update!');
+        var newBundle = watcher.bundle();
 
-				newBundle
-					.pipe(source('app.js'))
-					.pipe(gulp.dest('./build/'));
-				console.log('Updated in ' + (Date.now() - updateStart) + 'ms');
-				// rebundle(bundler);
-			})
-			.bundle() // create the initial bundle when starting the task
-			.pipe(source('app.js'))
-			.pipe(gulp.dest('./build/'))
-			.pipe(connect.reload());
+        newBundle
+          .pipe(source('app.js'))
+          .pipe(gulp.dest('./build/'));
+        console.log('Updated in ' + (Date.now() - updateStart) + 'ms');
+        // rebundle(bundler);
+      })
+      .bundle() // create the initial bundle when starting the task
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('./build/'))
+      .pipe(connect.reload());
 
-	} else {
-		bundler.transform(babelify);
-	}
+  } else {
+    bundler.transform(babelify);
+  }
 
 
 
-	// else {
-	//   bundler
-	//   	.bundle()
-	//    .pipe(source('app.js'))
-	//   	.pipe(gulp.dest('./build/'));
-	// }
-	// function rebundle(bundler) {
-	//  bundler.transform(babelify);
-	// 	console.log('ohai');
-	//   return bundler.bundle()
-	//     .on('error', function(e) {
-	//       gutil.log('Browserify Error', e);
-	//     })
-	//     .pipe(source('app.js'))
-	//     .pipe(buffer())
-	//     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-	//     .pipe(sourcemaps.write()) // writes .map file
-	//     .pipe(gulp.dest('.tmp/app'))
-	//     // .pipe(gulpif(watch, browserSync.reload({
-	//       // stream: true,
-	//       // once: true
-	//     // })));
-	// }
+  // else {
+  //   bundler
+  //    .bundle()
+  //    .pipe(source('app.js'))
+  //    .pipe(gulp.dest('./build/'));
+  // }
+  // function rebundle(bundler) {
+  //  bundler.transform(babelify);
+  //  console.log('ohai');
+  //   return bundler.bundle()
+  //     .on('error', function(e) {
+  //       gutil.log('Browserify Error', e);
+  //     })
+  //     .pipe(source('app.js'))
+  //     .pipe(buffer())
+  //     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+  //     .pipe(sourcemaps.write()) // writes .map file
+  //     .pipe(gulp.dest('.tmp/app'))
+  //     // .pipe(gulpif(watch, browserSync.reload({
+  //       // stream: true,
+  //       // once: true
+  //     // })));
+  // }
 
-	// return rebundle(bro);
+  // return rebundle(bro);
 }
 
 
 
 // Clean build and temp directories
 gulp.task('clean', del.bind(
-	null, ['.tmp', 'build/*', '!build/.git'], {dot: true}
+  null, ['.tmp', 'build/*', '!build/.git'], {dot: true}
 ));
 
 
 gulp.task('scripts', function(){
-	var src = gulp.src('src/**/*.js');
-	return src
-		.pipe(gulp.dest('build'))
-		.pipe(connect.reload());
+  var src = gulp.src('src/**/*.js');
+  return src
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
 });
 
 
 
 gulp.task('index', function() {
-	var target = gulp.src('src/index.html');
-	// var sources = gulp.src(['./src/**/*.js', './src/**/*.css'])
+  var target = gulp.src('src/index.html');
 
-	// return target.pipe(inject(sources))
-		// .pipe(gulp.dest('src'));
-	target
-		.pipe(gulp.dest('build'))
-		.pipe(connect.reload());
+  return target
+    .pipe(gulp.dest('build'))
+    .pipe(connect.reload());
 });
 
 
 
 // copy libs/*
 gulp.task('libs', function() {
-	src.assets = [
-		'src/libs/**'
-	];
+  src.assets = [
+    'src/libs/**'
+  ];
 
-	return gulp.src(src.assets)
-		.pipe($.changed('build')) // only changed files
-		.pipe(gulp.dest('build/libs')) // pipe them to our destination
-		.pipe($.size({title: 'libs'}))
-		.pipe(connect.reload()); // display sizes
+  return gulp.src(src.assets)
+    .pipe($.changed('build')) // only changed files
+    .pipe(gulp.dest('build/libs')) // pipe them to our destination
+    .pipe($.size({title: 'libs'}))
+    .pipe(connect.reload()); // display sizes
 });
 
 
 
 // copy components/*
 gulp.task('components', function() {
-	src.assets = [
-		'src/components/**'
-	];
+  src.assets = [
+    'src/components/**'
+  ];
 
-	return gulp.src(src.assets)
-		.pipe($.changed('build')) // only changed files
-		.pipe(gulp.dest('build/components')) // pipe them to our destination
-		.pipe($.size({title: 'components'})) // display sizes
-		.pipe(connect.reload());
+  return gulp.src(src.assets)
+    .pipe($.changed('build')) // only changed files
+    .pipe(gulp.dest('build/components')) // pipe them to our destination
+    .pipe($.size({title: 'components'})) // display sizes
+    .pipe(connect.reload());
 });
 
 
 // copy bower components into /libs/bower_components
 gulp.task('bower', function() {
-	src.assets = [
-		'bower_components/**'
-	];
+  src.assets = [
+    'bower_components/**'
+  ];
 
-	return gulp.src(src.assets)
-		.pipe($.changed('build')) // only changed files
-		.pipe(gulp.dest('build/libs/bower_components')) // pipe them to our destination
-		.pipe($.size({title: 'bower'})) // display sizes
-		.pipe(connect.reload())
+  return gulp.src(src.assets)
+    .pipe($.changed('build')) // only changed files
+    .pipe(gulp.dest('build/libs/bower_components')) // pipe them to our destination
+    .pipe($.size({title: 'bower'})) // display sizes
+    .pipe(connect.reload());
 });
 
 
 // copy static assets
 gulp.task('assets', function() {
-	src.assets = [
-		'src/assets/**'
-	];
+  src.assets = [
+    'src/assets/**'
+  ];
 
-	return gulp.src(src.assets)
-		.pipe($.changed('build')) // only changed files
-		.pipe(gulp.dest('build/assets')) // pipe them to our destination
-		.pipe($.size({title: 'assets'})) // display sizes
-		.pipe(connect.reload());
+  return gulp.src(src.assets)
+    .pipe($.changed('build')) // only changed files
+    .pipe(gulp.dest('build/assets')) // pipe them to our destination
+    .pipe($.size({title: 'assets'})) // display sizes
+    .pipe(connect.reload());
 });
 
 
 // SCSS and CSS
 gulp.task('css', function() {
-	return gulp.src('src/**/*.{css,scss}')
-	.pipe($.plumber())
-	.pipe($.sass({
-		sourceMap: !RELEASE,
-		sourceMapBasepath: __dirname
-	}))
-	.on('error', console.error.bind(console))
-	// .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
-	// .pipe($.csscomb())
-	// .pipe($.if(RELEASE, $.minifyCss()))
-	.pipe(concat('main.css'))
-	.pipe(gulp.dest('build/css'))
-	.pipe($.size({title: 'styles'}))
-	.pipe(connect.reload());
+  return gulp.src('src/**/*.{css,scss}')
+    .pipe($.plumber())
+    .pipe($.sass({
+      sourceMap: !RELEASE,
+      sourceMapBasepath: __dirname
+    }))
+    .on('error', console.error.bind(console))
+    // .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
+    // .pipe($.csscomb())
+    // .pipe($.if(RELEASE, $.minifyCss()))
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('build/css'))
+    .pipe($.size({title: 'styles'}))
+    .pipe(connect.reload());
 });
 
 // Open the app in a browser
 gulp.task('open', function () {
-	var options = {
-		url: 'http://localhost:9090/'
-	}
+  var options = {
+    url: 'http://localhost:9090/'
+  };
 
-	gulp.src('./build/index.html')
-	.pipe(open('', options));
-})
+  return gulp.src('./build/index.html')
+    .pipe(open('', options));
+});
 
 
 gulp.task('default', function () {
-  gulp.start('serve');
+  return gulp.start('serve');
 });
 
 gulp.task('serve', function () {
@@ -250,6 +247,6 @@ gulp.task('build-watch', ['clean'], function(cb) {
 });
 
 gulp.task('build', ['clean'], function(cb) {
-	runSequence(['browserify', 'css', 'assets', 'libs', 'bower', 'components', 'index'], cb);
+  runSequence(['browserify', 'css', 'assets', 'libs', 'bower', 'components', 'index'], cb);
 });
 
